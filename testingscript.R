1bfb3d54bda46b23a0b938096ff4732ad93afdc2
Deeana Nasrulai <deeananasrulai@Deeanas-MacBook-Pro.local>
2022-10-10 15:55:40 -0700

    done

---
+++ testingscript.R
@@ -0,0 +1,13 @@
+# creating variables
+
+my_var <- 10
+
+# create a function that will return the cubed result of a given number x
+
+number <- function(x){
+  answer <- x^3
+  return(answer)
+}
+
+number(3)
+cubed_output <- number(3)
